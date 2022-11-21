3c81f596797f9e9b813154d50ee2822599547e74
Drilon Syla <drilonsyla123@gmail.com>
2022-11-21 22:14:25 +0100

    Inserted File Upload

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = new express();
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 mongoose.connect('mongodb://localhost:27017/Blog_Db', { useNewUrlParser: true })
 const ejs = new require('ejs');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const BlogPost = require('./models/BlogPost.js')
@@ -13,7 +15,7 @@ const BlogPost = require('./models/BlogPost.js')
 app.get('/', async (req, res) => {
     const blogposts = await BlogPost.find({});
     res.render('index', {
-         blogposts
+        blogposts
     });
 });
 app.get("/about", (req, res) => {
@@ -22,18 +24,21 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
     res.render('contact');
 });
-app.get("/post/:id",async (req, res) => {
-   blogpost= await  BlogPost.findById(req.params.id)
-    res.render('post',{
+app.get("/post/:id", async (req, res) => {
+    blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', {
         blogpost
     });
 });
 app.get("/posts/new", (req, res) => {
     res.render('create');
 });
-app.post("/posts/store", async (req, res) => {
-    await BlogPost.create(req.body)
-    res.redirect('/')
+app.post("/posts/store", (req, res) => {
+    let image = req.files.image;
+    image.mv(path.resolve(__dirname, 'public/assets/img', image.name), async (error) => {
+        await BlogPost.create({...req.body,image:'/assets/img/'+image.name})
+        res.redirect('/')
+    })
 });
 
 app.listen(4000, () => {
