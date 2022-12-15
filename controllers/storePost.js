7070ef24360317868be16e90984865aff242970b
Drilon Syla <drilonsyla123@gmail.com>
2022-12-15 01:52:49 +0100

    added summernote to insert posts

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -2,8 +2,9 @@ const BlogPost = require('../models/BlogPost.js');
 const path = require('path');
 module.exports =  (req, res) => {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, 'public/assets/img', image.name), async (error) => {
-        await BlogPost.create({ ...req.body, image: '/assets/img/' + image.name })
+    image.mv(path.resolve(__dirname,'..', 'public/assets/img', image.name), async (error) => {
+        console.log(error);
+        await BlogPost.create({ ...req.body, image: '/assets/img/' + image.name,userid:req.session.userId })
         res.redirect('/')
     })
 };
